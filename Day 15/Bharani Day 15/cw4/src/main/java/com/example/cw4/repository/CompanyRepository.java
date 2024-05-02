013a28b7a981e9773e70633863db79dbb8a25189
Karthickkumar R <150213464+Rkarthick182005@users.noreply.github.com>
2024-05-02 07:55:39 +0530

    Delete Day 15/Bharani Day 15 directory

--- Day 15/Bharani Day 15/cw4/src/main/java/com/example/cw4/repository/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.cw4.repository;
-
-import com.example.cw4.model.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CompanyRepository extends JpaRepository<Company, Integer> {
-}
